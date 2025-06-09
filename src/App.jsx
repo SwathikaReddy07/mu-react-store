66729412672c08ae3da0820fe3432e7cd74db22c
SwathikaReddy07 <swathikareddytamidela@gmail.com>
2025-06-09 14:23:07 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header';
+import Footer from './Footer';
+import Home from './assets/Home';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+    
+    </div>
+    )
+}
+
+export default App
